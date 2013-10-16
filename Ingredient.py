bc4cdd28104a9bee3b84c112801a65669dd691e0
Marie Chatfield <marie.chatfield@gmail.com>
2013-10-16 00:27:02 -0500

    Update Ingredient to use Fractions instead of tuples, and add getChoices method.

--- Ingredient.py
+++ Ingredient.py
@@ -1,4 +1,5 @@
 import random
+import fractions
 
 """
 Ingredient class.
@@ -24,6 +25,11 @@ class Ingredient:
         self.name = name
         self.denom = denom
 
+        # choose a random numerator from the range 2 - denom
+        temp = range(2, self.denom)
+        random.shuffle(temp)
+        self.num = random.choice(temp)
+
 
     """
     Return the name of the Ingredient.
@@ -44,13 +50,74 @@ class Ingredient:
     def getDenominator (self):
         return self.denom
 
-
     """
-    Return a random fraction to use as the Ingredient's amount.
+    Return the numerator of the Ingredient.
 
     Returns:
-    (int, int) fraction -- a tuple of integers that represent numerator and denominator
+    int num -- the numerator of the Ingredient
     """
-    def getRandomAmount (self):
-        num = random.choice(range(1, self.denom))
-        return (num, self.denom)
+    def getNumerator (self):
+        return self.num
+
+    """
+    Return the Fraction representation of the Ingredient's amount.
+
+    Returns:
+    Fraction fraction -- the Fraction represented by num / denom
+    """
+    def getFraction (self):
+        return fractions.Fraction(self.num, self.denom)
+
+    """
+    Return a list of 4 choices that the user will select from to match
+    the Ingredient's Fraction. These choices are guaranteed to have
+    at least one combination (of either 2 or 3 fractions) that will add up to
+    the Fraction of the Ingredient.
+
+    Returns:
+    Fraction[] choices -- a list of four random Fractions to choose from
+    """
+    def getChoices (self):
+        choices = []
+        # make sure that the fraction itself cannot be a possible choice
+        allOptions = range(1, self.denom)
+        allOptions.remove(self.num)
+
+        # first choose a random number between 1 and the numerator
+        optionsLessThanNum = range(1, self.num)
+        choices.append(random.choice(optionsLessThanNum))
+        # do not allow this first option to appear twice
+        allOptions.remove(choices[0])
+        optionsLessThanNum.remove(choices[0])
+
+        # if there are more numbers between 1 and the numerator,
+        # add another as a possible choice
+        if optionsLessThanNum:
+            choices.append(random.choice(optionsLessThanNum))
+            # if the first two choices do not sum up to the numerator,
+            # add the difference between numerator and the first choice.
+            if (choices[0] + choices[1]) != self.num:
+                choices.append(self.num - choices[0])
+            # else, add a random option to make sure that three choices
+            # have been added by this point
+            else:
+                choices.append(random.choice(allOptions))
+        # if there are no more numbers between 1 and the numerator,
+        # add the difference between the numerator and the first choice
+        # to make the winning combination, plus a random option to make
+        # sure that three choices have been added by this point
+        else:
+            choices.append(self.num - choices[0])
+            choices.append(random.choice(allOptions))
+
+        # if the third choice is still in allOptions, remove it
+        # to prevent excessive duplicates; then add one more random option
+        if choices[2] in allOptions:
+            allOptions.remove(choices[2])
+        choices.append(random.choice(allOptions))
+
+        # convert each number in choices to be a Fraction; shuffle and return
+        for i in range(len(choices)):
+            choices[i] = fractions.Fraction(choices[i], self.denom)
+        random.shuffle(choices)
+        return choices
